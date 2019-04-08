b451bd93c309ab8f623c18c547474ddc08922722
Pratik <prabhupratik27@gmail.com>
2019-04-08 17:37:06 +0530

    Removed node_modules

--- server/src/controllers/AuthServices.js
+++ server/src/controllers/AuthServices.js
@@ -3,16 +3,21 @@ const passwordhasher = require('password-hasher')
 
 module.exports = {
     async register (req,res) {
-        const {password} = req.body
+        try{
+            const {password} = req.body
 
-        const hash = passwordhasher.createHash('ssha512',password,new Buffer('83d88386463f0625', 'hex'))
+            const hash = passwordhasher.createHash('ssha512',password,new Buffer('83d88386463f0625', 'hex'))
 
-        const rfcHash = passwordhasher.formatRFC2307(hash)
+            const rfcHash = passwordhasher.formatRFC2307(hash)
 
-        req.body.password = rfcHash
+            req.body.password = rfcHash
 
-        const user = await User.create(req.body);
-
+            const user = await User.create(req.body);
+        } catch {
+            res.status(400).send({
+                error: "Could not register user"
+            })
+        }
 
     }
 }
